8427121eb459a5724a1872201a44e4eae5db0c66
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-08-26 03:02:08 +0530

    Intalled all the dependencies for the shadcn setup for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import { Button } from "./components/ui/button";
 
 function App() {
-  
-
   return (
     <>
-      
+      <div className="p-4 bg-blue-500 text-white">Hello, Tailwind!</div>
+      <Button className='m-4 w-36 active:scale-90 duration-300 transition-all'>Button</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
